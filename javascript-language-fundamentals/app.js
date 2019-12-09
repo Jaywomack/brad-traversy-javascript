ec94965e1b8ed400f515953c189d1edff5b94338
jaywomack <womackjw@hotmail.com>
2019-12-08 20:38:16 -0800

    Started JS review

---
+++ javascript-language-fundamentals/app.js
@@ -0,0 +1,45 @@
+/*
+*
+**
+***
+****
+*****
+*/
+
+
+// console.log('hello world!')
+// console.log(123)
+// console.log(true)
+// var greeting = 'hello'
+// console.log(greeting)
+// console.log([1,2,3,4])
+// console.log({a:1, b:2})
+// console.table({a:1, b:2})
+// console.error('This is an error')
+// console.clear()
+// console.warn('this is a warning')
+// // Will run a timer on how long it takes to execute the functions
+// console.time('hello')
+// console.log('hello world!')
+// console.log('hello world!')
+// console.log('hello world!')
+// console.log('hello world!')
+// console.log('hello world!')
+// console.log('hello world!')
+
+// console.timeEnd('hello')
+
+//Declaring variables
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Init car
+var greeting;
+console.log(greeting)
+greeting = 'Hello'
+console.log(greeting)
+
+
