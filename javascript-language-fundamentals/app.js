9a1108e6a8ea83d4b6bf532935f1e2e9cc9f97f9
jaywomack <womackjw@hotmail.com>
2019-12-09 21:49:02 -0800

    finsihed object literals

--- javascript-language-fundamentals/app.js
+++ javascript-language-fundamentals/app.js
@@ -312,63 +312,112 @@
  * Arrays and array methods
  */
 
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22, 45, 33, 76, 54);
-const fruit = ["Apple", "Banana", "Orange", "Pear"];
-const mixed = [22, "Hello", true, undefined, null, { a: 1, b: 1 }, new Date()];
+// const numbers = [43, 56, 33, 23, 44, 36, 5];
+// const numbers2 = new Array(22, 45, 33, 76, 54);
+// const fruit = ["Apple", "Banana", "Orange", "Pear"];
+// const mixed = [22, "Hello", true, undefined, null, { a: 1, b: 1 }, new Date()];
+
+// let val;
+
+// // Get array length
+// // val = numbers.length;
+// // // Check if is array
+// // val = Array.isArray(numbers);
+// // // get a single value
+// // val = numbers[3];
+// // val = numbers[0];
+
+// // // Insert into array
+// // numbers[2] = 100;
+// // // Find index of value
+// // val = numbers.indexOf(36);
+
+// //Mutating arrays
+// // Add on to end
+// // numbers.push(250);
+// // // Add on to front
+// // numbers.unshift(120);
+// // // take of from end
+// // numbers.pop();
+// // // take off from front
+// // numbers.shift();
+// // // Splice values
+// // numbers.splice(1, 3);
+// // // Reverse array
+// // numbers.reverse();
+
+// // Concatenate array
+// val = numbers.concat(numbers2);
+
+// // // Sorting array
+// // val = fruit.sort();
+// // val = numbers.sort();
+
+// // // Use the 'compare' function
+// // val = numbers.sort(function(x, y) {
+// //   return x - y;
+// // });
+
+// // // Reverse sort
+// // val = numbers.sort(function(x, y) {
+// //   return y - x;
+// // });
+
+// // Find
+// function under50(num) {
+//   return num < 50;
+// }
+
+// val = numbers.find(under50);
+
+// console.log(numbers);
+// console.log(val);
+
+/******
+ *
+ * Object Literals
+ */
+
+const person = {
+  firstName: "Steve",
+  lastName: "Smith",
+  age: 30,
+  email: "steve@aol.com",
+  hobbies: ["music", "sports"],
+  address: {
+    city: "Miami",
+    state: "FL"
+  },
+  getBirthYear: function() {
+    return 2017 - this.age;
+  }
+};
 
 let val;
 
-// Get array length
-// val = numbers.length;
-// // Check if is array
-// val = Array.isArray(numbers);
-// // get a single value
-// val = numbers[3];
-// val = numbers[0];
+val = person;
+// Get specific value
+val = person.firstName;
+val = person["lastName"];
+val = person.age;
+val = person.hobbies;
+val = person.address.state;
+val = person.address["city"];
+val = person.getBirthYear();
 
-// // Insert into array
-// numbers[2] = 100;
-// // Find index of value
-// val = numbers.indexOf(36);
+console.log(val);
 
-//Mutating arrays
-// Add on to end
-// numbers.push(250);
-// // Add on to front
-// numbers.unshift(120);
-// // take of from end
-// numbers.pop();
-// // take off from front
-// numbers.shift();
-// // Splice values
-// numbers.splice(1, 3);
-// // Reverse array
-// numbers.reverse();
+const people = [
+  { name: "John", age: 30 },
+  { name: "Mike", age: 23 }
+];
 
-// Concatenate array
-val = numbers.concat(numbers2);
-
-// // Sorting array
-// val = fruit.sort();
-// val = numbers.sort();
-
-// // Use the 'compare' function
-// val = numbers.sort(function(x, y) {
-//   return x - y;
-// });
-
-// // Reverse sort
-// val = numbers.sort(function(x, y) {
-//   return y - x;
-// });
-
-// Find
-function under50(num) {
-  return num < 50;
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
 }
 
-val = numbers.find(under50);
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].age);
+}
 
-console.log(numbers);
-console.log(val);
+console.log(people.map(person => person.age));
