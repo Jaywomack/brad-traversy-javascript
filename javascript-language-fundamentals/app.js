a1292ccb445fb6339a1c8a41af98745ca484824d
jaywomack <womackjw@hotmail.com>
2019-12-10 17:44:04 -0800

    finished general loops

--- javascript-language-fundamentals/app.js
+++ javascript-language-fundamentals/app.js
@@ -615,19 +615,110 @@ const square = function(x = 3) {
 
 // PROPERTY METHODS
 
-const todo = {
-  add: function() {
-    console.log("Add todo..");
-  },
-  edit: function(id) {
-    console.log(`Edit todo ${id}`);
-  }
+// const todo = {
+//   add: function() {
+//     console.log("Add todo..");
+//   },
+//   edit: function(id) {
+//     console.log(`Edit todo ${id}`);
+//   }
+// };
+
+// todo.delete = function() {
+//   console.log("Delete todo");
+// };
+
+// todo.add();
+// todo.edit(22);
+// todo.delete();
+
+/***
+ * General Loops
+ * THREE MOST POPULAR
+ * FOR LOOPS    // Use when you know how many times it will run
+ * WHILE LOOPS // Use when it is unclear how many times it will run
+ * DO WHILE LOOPS
+ */
+
+// FOR LOOP
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`Number : ${i}`);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 2) {
+//     console.log(`2 is my favorite number`);
+//     continue;
+//   }
+//   if (i === 5) {
+//     console.log(`Stop the loop`);
+//     break;
+//   }
+//   console.log(`Number : ${i}`);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(`Number : ${i}`);
+//   i++;
+// }
+
+// DO WHILE LOOP
+// The DO WHILE LOOP will always run once
+// let i = 0;
+// do {
+//   console.log(`Number: ${i}`);
+//   i++;
+// } while (i < 10);
+
+// LOOP THROUGH ARRAYS
+
+const cars = ["Ford", "Chevy", "Honda", "Toyota"];
+
+// for (let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// fOREACH
+// cars.forEach(car => console.log(car));
+// cars.forEach(function(car, index) {
+//   console.log(`${index} : ${car}`);
+// });
+// MAP
+
+// let americanCars = cars.map(car => {
+//   if (car === "Ford" || car === "Chevy") {
+//     console.log(car);
+//   }
+// });
+
+// const users = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Sara" },
+//   { id: 3, name: "Karen" }
+// ];
+
+// const names = users.map(function(user) {
+//   return user.name;
+// });
+// console.log(names );
+// const ids = users.map(user => {
+//   return user.id;
+// });
+
+// console.log(ids);
+
+// FOR IN LOOP
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 40
 };
 
-todo.delete = function() {
-  console.log("Delete todo");
-};
-
-todo.add();
-todo.edit(22);
-todo.delete();
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
