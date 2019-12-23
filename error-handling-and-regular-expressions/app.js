4c6b0e1b25d2c90fb5a73533e2b156ac24c32639
jaywomack <womackjw@hotmail.com>
2019-12-22 17:12:21 -0800

    Working on Error handling and regular expressions

---
+++ error-handling-and-regular-expressions/app.js
@@ -0,0 +1,45 @@
+/***
+ *
+ * ERROR HANDLING WITH TRY CATCH
+ */
+
+// const user = { email: "jdoe@gmail.com" };
+
+// try {
+//   // Produce a Reference Error
+//   // myFunction();
+
+//   // Produce a type error
+//   // null.myFunction();
+
+//   // Produce syntax error
+//   // eval("hello World");
+
+//   // Will produce a URIError
+//   // decodeURIComponent("%");
+
+//   if (!user.name) {
+//     // throw "User has no name";
+//     throw new SyntaxError("User has no name");
+//   }
+// } catch (e) {
+//   // console.log(`${e.name} its Null dummy`);
+//   console.log(`User Error: ${e.message}`);
+//   // console.log(e.message);
+//   // console.log(e.name);
+//   // console.log(e instanceof ReferenceError);
+//   // console.log(e instanceof TypeError);
+// } finally {
+//   // console.log("Finally runs regardless of result");
+// }
+// // try catch will allow the script to continue
+
+// // console.log(" Program Continues");
+
+/****
+ *
+ *
+ * REGULAR EXPRESSIONS
+ *
+ * *****
+ */
